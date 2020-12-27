3cfd03bd1d06ad52f7cac95bc4e4b2c01b8df7b7
gissandrogama <gissandrogama@gmail.com>
2020-12-27 16:42:55 -0300

    simplifying functions ;

--- lib/car.ex
+++ lib/car.ex
@@ -4,10 +4,29 @@ defmodule Car do
 
   @car %{:novo => "novo.txt", :seminovo => "semi.txt"}
 
-  def buscar_carro(chassi) do
-    (read(:novo) ++ read(:seminovo))
+  def buscar_carro(chassi, key \\ :all) do
+    buscar(chassi, key)
+  end
+
+  defp buscar(chassi, :novo) do
+    carros_novo()
     |> Enum.find(fn carro -> carro.chassi == chassi end)
   end
+
+  defp buscar(chassi, :seminovo) do
+    carros_seminovo()
+    |> Enum.find(fn carro -> carro.chassi == chassi end)
+  end
+
+  defp buscar(chassi, :all) do
+    carros()
+    |> Enum.find(fn carro -> carro.chassi == chassi end)
+  end
+
+  def carros(), do: read(:novo) ++ read(:seminovo)
+  def carros_novo(), do: read(:novo)
+  def carros_seminovo(), do: read(:seminovo)
+
 
   def cadastrar(ano, marca, modelo, cambio, combustivel, cor, portas, chassi, tipo \\ :novo) do
     case buscar_carro(chassi) do
