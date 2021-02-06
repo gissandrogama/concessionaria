bbcaaa2541e3458b1b0916efa49e2d93845be361
gissandrogama <gissandrogama@gmail.com>
2021-02-06 10:03:31 -0300

    filtro de carros comprados em relatorio

--- lib/relatorio.ex
+++ lib/relatorio.ex
@@ -1,6 +1,9 @@
 defmodule Relatorio do
   def imprimir(mes, ano) do
     Car.carros_novo()
+    |> Enum.filter(fn carro ->
+      carro.tipo.comprado == true
+    end)
     |> Enum.map(&List.first(&1.registros))
     |> buscar_registro_mes(mes, ano)
   end
