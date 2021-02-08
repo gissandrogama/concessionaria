22773ae65d8bc71de3d4a6b0615f1aeb3805e4c4
gissandrogama <gissandrogama@gmail.com>
2021-02-08 19:32:26 -0300

    centralizando em concessionaria

--- lib/concessionaria.ex
+++ lib/concessionaria.ex
@@ -8,7 +8,31 @@ defmodule Concessionaria do
     File.write!("semi.txt", :erlang.term_to_binary([]))
   end
 
-  def cadastrar_carro(ano, marca, modelo, cambio, combustivel, cor, portas, chassi, tipo \\ :novo) do
+  def cadastrar_carro(ano, marca, modelo, cambio, combustivel, cor, portas, chassi, tipo) do
     Car.cadastrar(ano, marca, modelo, cambio, combustivel, cor, portas, chassi, tipo)
+  end
+
+  def comprar(chassi, data, valor, tipo) do
+    case tipo do
+      :novo ->
+        Novo.compra(chassi, data, valor)
+
+      :seminovo ->
+        Seminovo.compra(chassi, data, valor)
+    end
+  end
+
+  def vender(chassi, data, valor, tipo) do
+    case tipo do
+      :novo ->
+        Novo.venda(chassi, data, valor)
+
+      :seminovo ->
+        Seminovo.venda(chassi, data, valor)
+    end
+  end
+
+  def imprimir_relatorio() do
+    
   end
 end
